b3402e307ee43b409d05ff6f2a681bf5b039cefb
Luciano Pupp <llpupp@hotmail.com>
2022-06-27 15:46:59 -0300

    usando custom commands

--- cypress/e2e/pre-cadastro.cy.js
+++ cypress/e2e/pre-cadastro.cy.js
@@ -11,18 +11,24 @@ describe('Funcionalidade Pré Cadastro', () => {
     it('Deve completar o pré cadastro com sucesso', () => {
         let nomeFaker = faker.name.firstName()
         let sobrenomeFaker = faker.name.lastName()
-
         let emailFaker = faker.internet.email(nomeFaker)
 
         cy.get('#reg_email').type(emailFaker)
         cy.get('#reg_password').type('teste@teste.com')
         cy.get(':nth-child(4) > .button').click()
+
         cy.get('.woocommerce-MyAccount-navigation-link--edit-account > a').click()
         cy.get('#account_first_name').type(nomeFaker)
         cy.get('#account_last_name').type(sobrenomeFaker)
         cy.get('.woocommerce-Button').click()
+
         cy.get('.woocommerce-message').should('contain', 'Detalhes da conta modificados com sucesso')
-        
+
     });
-    
+    it.only('Deve completar o pré cadastro com sucesso usando Comandos customizados', () => {
+        let emailFaker2 = faker.internet.email()
+        cy.preCadastro(emailFaker2, 'senha!@#forte', 'Luciano', 'Pupp')
+        cy.get('.woocommerce-message').should('contain', 'Detalhes da conta modificados com sucesso')
+    });
+
 });
--- cypress/e2e/produtos.cy.js
+++ cypress/e2e/produtos.cy.js
@@ -18,11 +18,18 @@ describe('Funcionalidade Página de Produtos', () => {
         var quantidade = 10
         cy.get('[class="product-block grid"]')
             .contains('Aero Daily Fitness Tee').click()
-            cy.get('.button-variable-item-S').click()
-            cy.get('.button-variable-item-Brown').click()
-            cy.get('.input-text').clear().type(quantidade)
-            cy.get('.single_add_to_cart_button').click()
-            cy.get('.dropdown-toggle > .mini-cart-items').should('contain', quantidade)
-            cy.get('.woocommerce-message').should('contain', quantidade + ' × “Aero Daily Fitness Tee” foram adicionados no seu carrinho.')
+        cy.get('.button-variable-item-S').click()
+        cy.get('.button-variable-item-Brown').click()
+        cy.get('.input-text').clear().type(quantidade)
+        cy.get('.single_add_to_cart_button').click()
+
+        cy.get('.dropdown-toggle > .mini-cart-items').should('contain', quantidade)
+        cy.get('.woocommerce-message').should('contain', quantidade + ' × “Aero Daily Fitness Tee” foram adicionados no seu carrinho.')
     });
-});
+    it('Deve adicionar produtos ao carrinho - Usando Comando customizado', () => {
+        cy.addProdutos('Aero Daily Fitness Tee', 'XS', 'Brown', 2)
+    });
+    it('Deve adicionar produtos ao carrinho - Usando Comando customizado', () => {
+        cy.addProdutos('Aether Gym Pant', '33', 'Green', 5)
+    });
+})
