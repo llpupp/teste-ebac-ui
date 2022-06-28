64eea212d2432d2131af3f80cb7e9932e135e8da
Luciano Pupp <llpupp@hotmail.com>
2022-06-28 10:02:04 -0300

    Adição do EndFat + PageObjects

--- cypress/e2e/endereco.cy.js
+++ cypress/e2e/endereco.cy.js
@@ -1,16 +1,17 @@
 /// <reference types="cypress" />
+import EnderecoPage from '../support/page-objects/endereco.page'
 
 describe('Funcionalidade Endereços - Faturamento e Entrega', () => {
-beforeEach(() => {
-    cy.visit('minha conta')
-    cy.fixture('perfil').then(dados =>{
-        cy.login(dados.usuario, dados.senha)
-    })
-    
- });
+    beforeEach(() => {
+        cy.visit('minha conta')
+        cy.fixture('perfil').then(dados => {
+            cy.login(dados.usuario, dados.senha)
+        })
 
-    it('Deve fazer cadastro de faturamento com sucesso', () => {
-        
-            //cadastro de endereço
+    });
+
+    it.only('Deve fazer cadastro de faturamento com sucesso', () => {
+        EnderecoPage.editarEnderecoFaturamento('Flavio', 'Cavalcante', 'Embraer', 'Brasil', 'Av Brasil', '3100', 'Sorocaba', 'São Paulo', '18098220', '015-3428.2022', 'flavio.cavalcante@embraer.com')
+        cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso')
     });
 });
---
+++ cypress/support/page-objects/endereco.page.js
@@ -0,0 +1,27 @@
+class EnderecoPage {
+
+    editarEnderecoFaturamento(nome, sobrenome, empresa, país, endereco, número, cidade, estado, cep, telefone, email) {
+        cy.get('.woocommerce-MyAccount-navigation-link--edit-address > a').click()
+        cy.get(':nth-child(1) > .title > .edit').click()
+        cy.get('#billing_first_name').clear().type(nome)
+        cy.get('#billing_last_name').clear().type(sobrenome)
+        cy.get('#billing_company').clear().type(empresa)
+        cy.get('#select2-billing_country-container').click().type(país).get('[aria-selected="true"]').click()
+        cy.get('#billing_address_1').clear().type(endereco)
+        cy.get('#billing_address_2').clear().type(número)
+        cy.get('#billing_city').clear().type(cidade)
+        cy.get('#select2-billing_state-container').click().type( estado + '{enter}')
+        cy.get('#billing_postcode').clear().type(cep)
+        cy.get('#billing_phone').clear().type(telefone)
+        cy.get('#billing_email').clear().type(email)   
+
+        cy.get(':nth-child(2) > .button').click()
+
+    }
+
+    editarEnderecoEntrega() {
+        //elementos + ações
+    }
+
+}
+export default new EnderecoPage()
