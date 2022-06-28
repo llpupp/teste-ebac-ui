5a6048fe11d421a53b68ccefa3624835411f58a1
Luciano Pupp <llpupp@hotmail.com>
2022-06-28 11:11:36 -0300

    Cadastro de Faturamento usando arq de dados - fixtures

--- cypress/e2e/endereco.cy.js
+++ cypress/e2e/endereco.cy.js
@@ -1,5 +1,6 @@
 /// <reference types="cypress" />
 import EnderecoPage from '../support/page-objects/endereco.page'
+const dadosEndereco = require('../fixtures/endereco.json')
 
 describe('Funcionalidade Endereços - Faturamento e Entrega', () => {
     beforeEach(() => {
@@ -10,8 +11,27 @@ describe('Funcionalidade Endereços - Faturamento e Entrega', () => {
 
     });
 
-    it.only('Deve fazer cadastro de faturamento com sucesso', () => {
+    it('Deve fazer cadastro de faturamento com sucesso', () => {
         EnderecoPage.editarEnderecoFaturamento('Flavio', 'Cavalcante', 'Embraer', 'Brasil', 'Av Brasil', '3100', 'Sorocaba', 'São Paulo', '18098220', '015-3428.2022', 'flavio.cavalcante@embraer.com')
         cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso')
     });
+
+    it('Deve fazer cadastro de faturamento com sucesso - Usando Arquivo de Dados', () => {
+        EnderecoPage.editarEnderecoFaturamento(
+            dadosEndereco[1].nome,
+            dadosEndereco[1].sobrenome,
+            dadosEndereco[1].empresa,
+            dadosEndereco[1].país,
+            dadosEndereco[1].endereco,
+            dadosEndereco[1].numero,
+            dadosEndereco[1].cidade,
+            dadosEndereco[1].estado,
+            dadosEndereco[1].cep,
+            dadosEndereco[1].telefone,
+            dadosEndereco[1].email
+
+        )
+        cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso')
+    });
+
 });
